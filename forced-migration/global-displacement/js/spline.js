7f0ab8660e349c3a55192e79385400c5689a98b3
Anne Lee Steele <anneleesteele@ilab-staffs-iMac.local>
2018-09-05 15:48:51 -0400

    global displacement scenarios spline chart: basic without SOURCE*

---
+++ forced-migration/global-displacement/js/spline.js
@@ -0,0 +1,55 @@
+
+$(function() {
+  $('#hcContainer').highcharts({
+    // Load Data in from Google Sheets
+    data: {
+      googleSpreadsheetKey: '1x48glCS0NQfb-UboZk9gRme01Tb76r-VYCvY-VJVw3k',
+      googleSpreadsheetWorksheet: 1
+    },
+    // General Chart Options
+    chart: {
+      zoomType: 'x',
+      type: 'spline'
+    },
+    // Chart Title and Subtitle
+    title: {
+      text: "Projected Global Displacement Scenarios By Millions of People"
+    },
+    subtitle: {
+      text: "Click and drag to zoom in"
+    },
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: "CSIS Special Task Force on the Global Migration Crisis | Source: NAME"
+    },
+    // Chart Legend
+    legend: {
+      title: {
+        text: 'Legend Title<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>'
+      },
+      align: 'center',
+      verticalAlign: 'bottom',
+      layout: 'horizontal'
+    },
+    // Y Axis
+    yAxis: {
+      title: {
+        text: "Y Axis Title"
+      }
+    },
+    // Additional Plot Options
+    plotOptions:
+    {
+      spline: {
+        marker: {
+          enabled: false,
+          symbol: "circle",
+          radius: 3
+        },
+        lineWidth: 3
+      }
+    }
+  });
+});
