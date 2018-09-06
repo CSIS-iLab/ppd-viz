9780a172e9ca0d90b32356c18f41f28f5b212605
Anne Lee Steele <anneleesteele@ilab-staffs-iMac.local>
2018-09-06 08:53:14 -0400

    cleaned up code before pull request

--- forced-migration/global-displacement/js/spline.js
+++ forced-migration/global-displacement/js/spline.js
@@ -1,30 +1,25 @@
 
 $(function() {
   $('#hcContainer').highcharts({
-    // Load Data in from Google Sheets
     data: {
       googleSpreadsheetKey: '1x48glCS0NQfb-UboZk9gRme01Tb76r-VYCvY-VJVw3k',
       googleSpreadsheetWorksheet: 1
     },
-    // General Chart Options
     chart: {
       zoomType: 'x',
       type: 'spline'
     },
-    // Chart Title and Subtitle
     title: {
       text: "Projected Global Displacement Scenarios By Millions Of People"
     },
     subtitle: {
       text: "Click and drag to zoom in"
     },
-    // Credits
     credits: {
       enabled: true,
       href: false,
       text: "CSIS Special Task Force on the Global Migration Crisis"
     },
-    // Chart Legend
     legend: {
       title: {
         text: '<span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>'
@@ -33,13 +28,11 @@ $(function() {
       verticalAlign: 'bottom',
       layout: 'horizontal'
     },
-    // Y Axis
     yAxis: {
       title: {
         text: "Millions of People"
       }
     },
-    // Additional Plot Options
     plotOptions:
     {
       spline: {
