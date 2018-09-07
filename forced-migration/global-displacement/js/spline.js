705fb552ac6ed7b7c59ab09b927bc103d58c3c6a
Anne Lee Steele <anneleesteele@ilab-staffs-iMac.local>
2018-09-07 12:00:41 -0400

    kept x-axis whole numbers, changed title, vanilla js

--- forced-migration/global-displacement/js/spline.js
+++ forced-migration/global-displacement/js/spline.js
@@ -1,48 +1,52 @@
-
-$(function() {
-  $('#hcContainer').highcharts({
+let hcContainer = document.getElementById('#hcContainer')
+let spline = new Highcharts.Chart({
     data: {
-      googleSpreadsheetKey: '1x48glCS0NQfb-UboZk9gRme01Tb76r-VYCvY-VJVw3k',
-      googleSpreadsheetWorksheet: 1
+        googleSpreadsheetKey: '1x48glCS0NQfb-UboZk9gRme01Tb76r-VYCvY-VJVw3k',
+        googleSpreadsheetWorksheet: 1
     },
     chart: {
-      zoomType: 'x',
-      type: 'spline'
+        zoomType: 'x',
+        type: 'spline',
+        renderTo: 'hcContainer'
     },
     title: {
-      text: "Projected Global Displacement Scenarios By Millions Of People"
+        text: "Projected Global Displacement Scenarios By Millions Of People"
     },
     subtitle: {
-      text: "Click and drag to zoom in"
+        text: "Click and drag to zoom in"
     },
     credits: {
-      enabled: true,
-      href: false,
-      text: "CSIS Special Task Force on the Global Migration Crisis"
+        enabled: true,
+        href: false,
+        text: "CSIS Special Task Force on the Global Migration Crisis"
     },
     legend: {
-      title: {
-        text: '<span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>'
-      },
-      align: 'center',
-      verticalAlign: 'bottom',
-      layout: 'horizontal'
+        title: {
+            text: '<span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>'
+        },
+        align: 'center',
+        verticalAlign: 'bottom',
+        layout: 'horizontal'
+    },
+    xAxis: {
+        allowDecimals: false
     },
     yAxis: {
-      title: {
-        text: "Millions of People"
-      }
+        title: {
+            text: "Millions of People"
+        }
     },
-    plotOptions:
-    {
-      spline: {
-        marker: {
-          enabled: false,
-          symbol: "circle",
-          radius: 3
-        },
-        lineWidth: 3
-      }
+    // tooltip: {
+    //     // valueDecimals: 0
+    // },
+    plotOptions: {
+        spline: {
+            marker: {
+                enabled: false,
+                symbol: "circle",
+                radius: 3
+            },
+            lineWidth: 3
+        }
     }
-  });
 });
